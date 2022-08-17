a37d20cfc77af7533e650f14d159ba06f37e87a3
JaehyeonPaak <91472722+JaehyeonPaak@users.noreply.github.com>
2022-08-17 20:14:35 +0900

    .

--- 2-1/login_process.php
+++ 2-1/login_process.php
@@ -9,17 +9,13 @@
   $result = mysqli_query($mysqli, $query);
   $row = mysqli_fetch_array($result);
 
-  if(!isset($row['user_id']) || $row['password'] != $password) { ?>
-    <div class="alert alert-primary" role="alert">
-      A simple primary alert—check it out!
-    </div>
-<?php
+  if(!isset($row['user_id']) || $row['password'] != $password) {
     header('Location: '.'/2-1/login.php');
   }
   else {
-      $user_class = $row['user_class'];
-      $_SESSION['user_id'] = $user_id;
-      $_SESSION['user_class'] = $user_class;
-      header('Location: '.'/2-1/index.php');
+    $user_class = $row['user_class'];
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['user_class'] = $user_class;
+    header('Location: '.'/2-1/index.php');
   }
  ?>
