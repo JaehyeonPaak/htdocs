9012a42eb3b2091e9a8dea3371fb786cf5bb2322
JaehyeonPaak <91472722+JaehyeonPaak@users.noreply.github.com>
2022-08-18 15:44:20 +0900

    .

--- 1-2/prime_number.php
+++ 1-2/prime_number.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>웹티즌 사전과제 【필수과제 1】 1-1</title>
+    <title>웹티즌 사전과제 【필수과제 1】 1-2</title>
   </head>
   <body>
+    <h1>소수 출력<h1>
     <form action='prime_number.php' method='GET'>
       <input type=text name='val1' placeholder='첫번째 자연수'>
       <input type=text name='val2' placeholder='두번째 자연수'>
---
+++ 1-3/tiling.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>웹티즌 사전과제 【필수과제 1】 1-3</title>
+  </head>
+  <body>
+    <h1>2xn 타일링<h1>
+    <form action='tiling.php' method='GET'>
+      <input type=text name='val' placeholder='1이상 1000이하 숫자 입력'>
+      <input type=submit value='입력'><br>
+    <form>
+  </body>
+</html>
+
+<?php
+  if(isset($_GET['val'])) {
+    $val = $_GET['val'];
+    echo fibonacci($val);
+  }
+
+  function fibonacci($val) {
+    if($val <= 1) {
+      return 1;
+    }
+    else {
+      return fibonacci($val - 1) + fibonacci($val - 2);
+    }
+  }
+ ?>
